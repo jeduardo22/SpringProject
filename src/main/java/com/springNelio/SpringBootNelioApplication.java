d8e6e125768770fc0ca9fca019152730fdbdd6d9
Eduardo <jeduardo.miranda@gmail.com>
2019-12-14 01:06:06 -0300

    Projeto criado

---
+++ src/main/java/com/springNelio/SpringBootNelioApplication.java
@@ -0,0 +1,13 @@
+package com.springNelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNelioApplication.class, args);
+	}
+
+}
