897242d557f1d5751a24285933ff9a04f1b9bad2
jeduardo22 <jeduardo.miranda@gmail.com>
2019-12-15 19:13:07 -0300

    Estados e Cidades

--- src/main/java/com/springNelio/SpringBootNelioApplication.java
+++ src/main/java/com/springNelio/SpringBootNelioApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.springNelio.domain.Categoria;
+import com.springNelio.domain.Cidade;
+import com.springNelio.domain.Estado;
 import com.springNelio.domain.Produto;
 import com.springNelio.repositories.CategoriaRepository;
+import com.springNelio.repositories.CidadeRepository;
+import com.springNelio.repositories.EstadoRepository;
 import com.springNelio.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -22,6 +26,11 @@ public class SpringBootNelioApplication implements CommandLineRunner {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootNelioApplication.class, args);
@@ -50,6 +59,19 @@ public class SpringBootNelioApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 
