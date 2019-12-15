2bfa5fac3c75b50259e132f7578b64677c2bbb1f
jeduardo22 <jeduardo.miranda@gmail.com>
2019-12-15 00:13:41 -0300

    Muitos para muitos

--- src/main/java/com/springNelio/SpringBootNelioApplication.java
+++ src/main/java/com/springNelio/SpringBootNelioApplication.java
@@ -1,13 +1,56 @@
 package com.springNelio;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootNelioApplication {
+import com.springNelio.domain.Categoria;
+import com.springNelio.domain.Produto;
+import com.springNelio.repositories.CategoriaRepository;
+import com.springNelio.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class SpringBootNelioApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootNelioApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// 
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 8000.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
 	}
 
 }
